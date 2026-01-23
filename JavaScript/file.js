16c1444bd53835bc75d0b3d49d97a52f155c7e35
Aditya Kauthkar <adityakauthkar2021.it@mmcoe.edu.in>
2026-01-23 13:44:58 +0000

    payment gateway fixed

--- JavaScript/file.js
+++ JavaScript/file.js
@@ -1,1 +1,13 @@
+//This file contains every fucking javascript details 
+// let a = 10 ; 
+// let b = 20 ;
+
+// console.log(a+b) ; 
+
+
+// concatenate strings 
+/* let m = "aditya"
+let n = "swaraj "
+console.log(m+n); */
+
 
